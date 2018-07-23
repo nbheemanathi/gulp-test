0601f26c4bdd2b72d9fe12bc0e4a03eb9d09f908
nbheema <bheemanathi.nihar@gmail.com>
2018-07-22 19:23:12 -0700

    side child menu

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,42 +2,69 @@
 Dashboard core JS file 
 ========================================================================== */
 
-$(document).ready(function ($) {
-    "use strict";
+$(document).ready(function($) {
+  "use strict";
 
-    //Reader mode (only for mobile)
-    $('#reader-mode-toggle').on("click", function () {
-        $('body').toggleClass('reader-mode');
-    });
+  //#region Render mode toggle(only for mobile)
+  $("#reader-mode-toggle").on("click", function() {
+    $("body").toggleClass("reader-mode");
+  });
+  //#endregion
 
-    //Main menu icon behaviour and push sidebar
-    $('.side-icon').on("click", function () {
-        $('.side-icon.is-active').removeClass('is-active');
-        $(this).addClass('is-active');
-        $('.child-menu').addClass('is-sidebar-translated');
-        $('.dashboard-nav').addClass('is-pushed');
-         //disable reader mode switch when sidebar is opened
-         $('.reader-switch label').addClass('is-disabled');
-         //child menu icon rotate active
-         $('.menu-wrapper .icon-box-toggle').addClass('active');
-    });
+  //#region Main menu icon behaviour and push sidebar
+  $(".side-icon").on("click", function() {
+    $(".side-icon.is-active").removeClass("is-active");
+    $(this).addClass("is-active");
+    $(".child-menu").addClass("is-sidebar-translated");
+    $(".dashboard-nav").addClass("is-pushed");
+    //disable reader mode switch when sidebar is opened
+    $(".reader-switch label").addClass("is-disabled");
+    //child menu icon rotate active
+    $(".menu-wrapper .icon-box-toggle").addClass("active");
+  });
+  //#endregion
 
-    //child menu close icon click
-    $('.menu-wrapper').on("click", function () {
-        $('.child-menu').toggleClass('is-sidebar-translated');
-        $('.dashboard-nav, #dashboard-wrapper').toggleClass('is-pushed');
-         //enable reader mode switch when sidebar is closed
-         $('.reader-switch label').removeClass('is-disabled');
-         $('.side-icon.is-active').removeClass('is-active');
-    });
+  //#region child menu close icon click
+  $(".menu-wrapper").on("click", function() {
+    $(".child-menu").toggleClass("is-sidebar-translated");
+    $(".dashboard-nav, #dashboard-wrapper").toggleClass("is-pushed");
+    //enable reader mode switch when sidebar is closed
+    $(".reader-switch label").removeClass("is-disabled");
+    $(".side-icon.is-active").removeClass("is-active");
+  });
+  //#endregion
 
-    //quickView toggle event
-    $('.chat-button').on("click", function () {
-        $('.quickview').toggleClass('is-active');
-    });
-    $('.cross-container').on("click", function () {
-        $('.quickview').toggleClass('is-active');
-    });
+  //#region quickView toggle event (temp)
+  $(".chat-button").on("click", function() {
+    $(".quickview").toggleClass("is-active");
+  });
+  $(".cross-container").on("click", function() {
+    $(".quickview").toggleClass("is-active");
+  });
+  //#endregion
 
+  //#region Side Child menu click
+  $(".sidebar-menu > li.have-children a.parent-link").on("click", function(i) {
+    i.preventDefault();
+    if (!$(this).parent().hasClass("active")) {
+        $(".sidebar-menu li ul").slideUp();
+        $(this).next().slideToggle();
+        $(".sidebar-menu li").removeClass("active");
+        $(this).parent().addClass("active");
+    } 
+    else {
+      $(this).next().slideToggle();
+      $(".sidebar-menu li").removeClass("active");
+    }
+  });
+  //#endregion
 
-})
+  //#region Data child menu setup
+  $('.main-menu ul li.side-icon').on("click", function(){
+      var menu_id = $(this).attr('data-child-menu');
+      $('.sidebar-menu.is-active').removeClass('is-active');
+      $("#" + menu_id).addClass('is-active');
+    })
+     //#endregion
+
+});
